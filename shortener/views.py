dd2591526e2bee85fb4a3980ac3595382d51d80e
jake-hong <hongse21@gmail.com>
2021-10-11 12:00:56 +0900

    Migrate shortener and Create views

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from shortener.models import Users
 
 # Create your views here.
+
+def index(request):
+    user = Users.objects.filter(username="admin").first()
+    email = user.email if user else "anonyumous user!"
+    print(email)
+    print(request.user.is_authenticated)
+    if request.user.is_authenticated is False:
+        email = "anonymous user!"
+        print(email)
+    return render(request, "base.html",{"welcome_msg":f"hello{user}"}
+    )
+
+def redirect_test(request):
+    print("GO redirect")
+    return redirect("index")
