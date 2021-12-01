12740568fbbd717d9bac6c533e14ef02c5fe4cc7
jake-hong <hongse21@gmail.com>
2021-12-01 12:28:35 +0900

    update Django board and login.html

--- shortener/forms.py
+++ shortener/forms.py
@@ -1,11 +1,14 @@
-from django import forms 
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from shortener.models import Users
+from django.forms.widgets import Widget
+from shortener.models import ShortenedUrls, Users
+from django.utils.translation import gettext_lazy as _
+
 
 class RegisterForm(UserCreationForm):
-    full_name = forms.CharField(max_length=30,required=False,help_text="Optional.",label="이름")
-    username = forms.CharField(max_length=30,required=False,help_text="Optional.",label="유저명")
-    email = forms.EmailField(max_length=254,help_text="Required. Inform a valid email address",label="이메일")
+    full_name = forms.CharField(max_length=30, required=False, help_text="Optional.", label="이름")
+    username = forms.CharField(max_length=30, required=False, help_text="Optional.", label="유저명")
+    email = forms.EmailField(max_length=254, help_text="Required. Inform a valid email address", label="이메일")
 
     class Meta:
         model = Users
@@ -17,6 +20,41 @@ class RegisterForm(UserCreationForm):
             "password2",
         )
 
-class AuthenticationFormWithInactiveUsersOkay(AuthenticationForm):
-    def confirm_login_allowed(self, user):
-        pass
+
+class LoginForm(forms.Form):
+    email = forms.CharField(
+        max_length=100, required=True, widget=forms.TextInput(attrs={"class": "form-control", "placeholer": "이메일"})
+    )
+    password = forms.CharField(
+        max_length=30, required=True, widget=forms.PasswordInput(attrs={"class": "form-control", "placeholer": "비밀번호"})
+    )
+    remember_me = forms.BooleanField(widget=forms.CheckboxInput)
+
+
+class UrlCreateForm(forms.ModelForm):
+    class Meta:
+        model = ShortenedUrls
+        fields = ["nick_name", "target_url"]
+        labels = {
+            "nick_name": _("별칭"),
+            "target_url": _("URL"),
+        }
+        widgets = {
+            "nick_name": forms.TextInput(attrs={"class": "form-control", "placeholder": "URL을 구분하기 위한 별칭"}),
+            "target_url": forms.TextInput(attrs={"class": "form-control", "placeholder": "포워딩될 URL"}),
+        }
+
+    def save(self, request, commit=True):
+        instance = super(UrlCreateForm, self).save(commit=False)
+        instance.created_by_id = request.user.id
+        instance.target_url = instance.target_url.strip()
+        if commit:
+            instance.save()
+        return instance
+
+    def update_form(self, request, url_id):
+        instance = super(UrlCreateForm, self).save(commit=False)
+        instance.target_url = instance.target_url.strip()
+        ShortenedUrls.objects.filter(pk=url_id, created_by_id=request.user.id).update(
+            target_url=instance.target_url, nick_name=instance.nick_name
+        )
--- shortener/views.py
+++ shortener/views.py
@@ -1,45 +1,98 @@
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, PasswordChangeForm
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import redirect, render 
-from shortener.models import Users
-from shortener.forms import RegisterForm
-from django.contrib.auth import authenticate,login, logout
+from django.shortcuts import redirect, render
+from shortener.models import ShortenedUrls, Users
+from shortener.forms import RegisterForm, LoginForm, UrlCreateForm
+from django.contrib.auth import authenticate, login, logout
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     # print(request.user.pay_plan.name)
     user = Users.objects.filter(id=request.user.id).first()
     email = user.email if user else "anonyumous user!"
-    print("Logged in?",  request.user.is_authenticated)
+    print("Logged in?", request.user.is_authenticated)
     # print(request.user.pay_plan.name)
-    
+
     if request.user.is_authenticated is False:
         email = "anonymous user!"
         print(email)
-    return render(request, "base.html",{"welcome_msg":f"hello"})
+    return render(request, "base.html", {"welcome_msg": f"hello"})
 
 
-@csrf_exempt # 사이트 위변조 방지 
-def get_user(request,user_id):
+def url_list(request):
+    get_list = ShortenedUrls.objects.order_by("-created_at").all()
+    return render(request, "url_list.html", {"list": get_list})
+
+
+@login_required
+def url_create(request):
+    msg = None
+    if request.method == "POST":
+        form = UrlCreateForm(request.POST)
+        if form.is_valid():
+            msg = f"{form.cleaned_data.get('nick_name')} 생성 완료!"
+            messages.add_message(request, messages.INFO, msg)
+            form.save(request)
+            return redirect("url_list")
+        else:
+            form = UrlCreateForm()
+    else:
+        form = UrlCreateForm()
+    return render(request, "url_create.html", {"form": form})
+
+
+@login_required
+def url_change(request, action, url_id):
+    if request.method == "POST":
+        url_data = ShortenedUrls.objects.filter(id=url_id)
+        if url_data.exists():
+            if url_data.first().created_by_id != request.user.id:
+                msg = "자신이 소유하지 않은 URL 입니다."
+            else:
+                if action == "delete":
+                    msg = f"{url_data.first().nick_name} 삭제 완료!"
+                    url_data.delete()
+                    messages.add_message(request, messages.INFO, msg)
+                elif action == "update":
+                    msg = f"{url_data.first().nick_name} 수정 완료!"
+                    form = UrlCreateForm(request.POST)
+                    form.update_form(request, url_id)
+
+                    messages.add_message(request, messages.INFO, msg)
+        else:
+            msg = "해당 URL 정보를 찾을 수 없습니다."
+
+    elif request.method == "GET" and action == "update":
+        url_data = ShortenedUrls.objects.filter(pk=url_id).first()
+        form = UrlCreateForm(instance=url_data)
+        return render(request, "url_create.html", {"form": form, "is_update": True})
+
+    return redirect("url_list")
+
+
+@csrf_exempt  # 사이트 위변조 방지
+def get_user(request, user_id):
     print(user_id)
-    
-    if request.method =="GET":
+
+    if request.method == "GET":
         abc = request.GET.get("abc")
         xyz = request.GET.get("xyz")
         user = Users.objects.filter(pk=user_id).first()
-        return render(request,"base.html",{"user":user,"params":[abc,xyz]})
-    
+        return render(request, "base.html", {"user": user, "params": [abc, xyz]})
+
     elif request.method == "POST":
         username = request.GET.get("username")
-        if username :
+        if username:
             user = Users.objects.filter(pk=user_id).update(username=username)
-            return JsonResponse(status=201, data = dict(msg="You just reached with post method"), safe =False)
+            return JsonResponse(status=201, data=dict(msg="You just reached with post method"), safe=False)
+
 
 def register(request):
-    if request.method =="POST":
+    if request.method == "POST":
         form = RegisterForm(request.POST)
         msg = "unvalid data"
 
@@ -48,33 +101,40 @@ def register(request):
             username = form.cleaned_data.get("username")
             raw_pw = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_pw)
-            login(request,user,backend='django.contrib.auth.backends.ModelBackend')
-            msg="로그인 성공"
-        return render(request, "register.html", {"form":form,"msg":msg})
+            login(request, user, backend="django.contrib.auth.backends.ModelBackend")
+            msg = "로그인 성공"
+        return render(request, "register.html", {"form": form, "msg": msg})
     else:
         form = RegisterForm()
-        return render(request,"register.html",{"form":form})  
+        return render(request, "register.html", {"form": form})
+
 
 def login_view(request):
-    msg = None
     is_ok = False
     if request.method == "POST":
-        form = AuthenticationForm(request, request.POST)
+        form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get("username")
+            email = form.cleaned_data.get("email")
             raw_password = form.cleaned_data.get("password")
-            user = authenticate(username=username, password=raw_password)
-            if user is not None:
-                login(request, user)
-                is_ok = True
-        else:
+            remember_me = form.cleaned_data.get("remember_me")
             msg = "올바른 유저ID와 패스워드를 입력하세요."
-    else:
-        form = AuthenticationForm()
+            try:
+                user = Users.objects.get(email=email)
+            except Users.DoesNotExist:
+                pass
+            else:
+                if user.check_password(raw_password):
+                    msg = None
+                    login(request, user)
+                    is_ok = True
+                    request.session["remember_me"] = remember_me
 
-    for visible in form.visible_fields():
-        visible.field.widget.attrs["placeholder"] = "유저ID" if visible.name == "username" else "패스워드"
-        visible.field.widget.attrs["class"] = "form-control"
+                    # if not remember_me:
+                    #     request.session.set_expiry(0)
+    else:
+        msg = None
+        form = LoginForm()
+    print("REMEMBER_ME: ", request.session.get("remember_me"))
     return render(request, "login.html", {"form": form, "msg": msg, "is_ok": is_ok})
 
 
@@ -82,10 +142,11 @@ def logout_view(request):
     logout(request)
     return redirect("index")
 
-# @login_required
+
+@login_required
 def list_view(request):
-    page= int(request.GET.get("p",1))
+    page = int(request.GET.get("p", 1))
     users = Users.objects.all().order_by("id")
-    paginator = Paginator(users,10)
+    paginator = Paginator(users, 10)
     users = paginator.get_page(page)
-    return render(request,"board.html",{"users":users})
+    return render(request, "board.html", {"users": users})
